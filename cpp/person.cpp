b38e328a5f1c0b91161b7b7f19294063c580faca
Z.X <903464207@qq.com>
2020-11-11 17:10:48 +0800

    no commit message

---
+++ cpp/person.cpp
@@ -0,0 +1,17 @@
+//#include "person.h"
+//#include <string>
+//#include <iostream>
+//#include <ctime>
+//using namespace std;
+//
+//class person
+//{
+//public:
+//	string name;
+//	int age;
+//
+//private:
+//	string ss;
+//
+//
+//};

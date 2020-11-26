5ce0963689fb7b742bff6b89b9a4942d214adcdb
Z.X <903464207@qq.com>
2020-11-26 22:24:16 +0800

    no commit message

--- controllers/TestCtrl.cc
+++ controllers/TestCtrl.cc
@@ -1,30 +1,47 @@
 #include "TestCtrl.h"
 #include "../models/Admin.h"
+#include <exception> 
 
 using namespace web;
-// using namespace drogon_model::v2;
+using namespace drogon_model::v2;
 
 
 void TestCtrl::name(const HttpRequestPtr &req,
              std::function<void (const HttpResponsePtr &)> &&callback) const
 {
-    auto clientPtr = drogon::app().getDbClient();
-    // auto f = clientPtr->execSqlAsyncFuture("select * from admin");
-    // auto r = f.get(); // Block until we get the result or catch the exception;
-    // std::cout << r.size() << " rows selected!" << std::endl;
-    // int i = 0;
-    //  for (auto row : r)
-    // {
-    //     std::cout << i++ << ": user name is " << row["real_name"].as<std::string>() << std::endl;
-    // }
+       
+    try{
+        auto clientPtr = drogon::app().getDbClient();
 
-    Mapper<Admin> mp(clientPtr);
-    // auto rr = mp.orderBy(Admin::Cols::_id).limit(25).offset(0).findAll();
-     auto rr = mp.findAll();
-    std::cout << rr.size() << " rows selected!" << std::endl;
+        // auto f = clientPtr->execSqlAsyncFuture("select * from admin");
+        // auto r = f.get(); // Block until we get the result or catch the exception;
+        // std::cout << r.size() << " rows zzzzzzzzzzzzzzzzzzzzzzzz!" << std::endl;
+        // int i = 0;
+        //  for (auto row : r)
+        // {
+        //     std::cout << i++ << ": user name is " << row["real_name"].as<std::string>() << std::endl;
+        // }
+
+        Mapper<Admin> mp(clientPtr);
+        // auto rr = mp.orderBy(Admin::Cols::_id).limit(25).offset(0).findAll();
+
+        std::vector<Admin> uu = mp.orderBy(Admin::Cols::_id).limit(25).offset(0).findAll();
+
+        // auto rr = mp.findAll();
+        std::cout << uu.size() << " rows xxxxxxxxxxxxxxxxxxxx!" << std::endl;
+
+        // int i = 0;
+        //  for (auto y : rr)
+        // {
+        //     std::cout << i++ << ": user name is " <<  *y.getRealName() << std::endl;
+        // }
+    }catch(std::exception &e){
+        std::cout << e.what() << std::endl;  
+    }
+    
 
     Json::Value ret;
-    ret["result"]="ok";
+    ret["result"]="33";
     ret["user_name"]="Jack";
     ret["gender"]=1;
     auto resp=HttpResponse::newHttpJsonResponse(ret);

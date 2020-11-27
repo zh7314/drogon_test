34b915d623385b1755150c511cdb09d358f98415
Z.X <903464207@qq.com>
2020-11-27 17:33:28 +0800

    no commit message

--- controllers/TestCtrl.cc
+++ controllers/TestCtrl.cc
@@ -4,15 +4,14 @@
 
 using namespace web;
 using namespace drogon_model::v2;
+using namespace drogon::orm;
 
 
 void TestCtrl::name(const HttpRequestPtr &req,
              std::function<void (const HttpResponsePtr &)> &&callback) const
 {
-       
     try{
         auto clientPtr = drogon::app().getDbClient();
-
         // auto f = clientPtr->execSqlAsyncFuture("select * from admin");
         // auto r = f.get(); // Block until we get the result or catch the exception;
         // std::cout << r.size() << " rows zzzzzzzzzzzzzzzzzzzzzzzz!" << std::endl;
@@ -21,25 +20,18 @@ void TestCtrl::name(const HttpRequestPtr &req,
         // {
         //     std::cout << i++ << ": user name is " << row["real_name"].as<std::string>() << std::endl;
         // }
-
         Mapper<Admin> mp(clientPtr);
-        // auto rr = mp.orderBy(Admin::Cols::_id).limit(25).offset(0).findAll();
+        // std::vector<Admin> uu = mp.orderBy(Admin::Cols::_id).limit(25).offset(0).findAll();
 
-        std::vector<Admin> uu = mp.orderBy(Admin::Cols::_id).limit(25).offset(0).findAll();
+        auto iii = mp.count();
+        std::cout << iii << " rows 111111111111111!" << std::endl;
 
-        // auto rr = mp.findAll();
-        std::cout << uu.size() << " rows xxxxxxxxxxxxxxxxxxxx!" << std::endl;
+        auto uu = mp.orderBy(Admin::Cols::_id).limit(5).offset(5).findAll();
+        std::cout << uu.size() << " rows 2222222222222222!" << std::endl;
 
-        // int i = 0;
-        //  for (auto y : rr)
-        // {
-        //     std::cout << i++ << ": user name is " <<  *y.getRealName() << std::endl;
-        // }
-    }catch(std::exception &e){
-        std::cout << e.what() << std::endl;  
+    }catch (const DrogonDbException &e){
+        std::cout << "error:" << e.base().what() << std::endl;
     }
-    
-
     Json::Value ret;
     ret["result"]="33";
     ret["user_name"]="Jack";

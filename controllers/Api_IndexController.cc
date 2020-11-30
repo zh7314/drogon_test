560df21a7049a874531d0700545fc3690a2a699f
Z.X <903464207@qq.com>
2020-11-30 14:20:25 +0800

    no commit message

--- controllers/Api_IndexController.cc
+++ controllers/Api_IndexController.cc
@@ -7,6 +7,8 @@ using namespace api;
 using namespace drogon_model::v2;
 using namespace drogon::orm;
 
+
+
 void Api_IndexController::index(const HttpRequestPtr &req,
                                 std::function<void(const HttpResponsePtr &)> &&callback)
 {
@@ -19,15 +21,35 @@ void Api_IndexController::index(const HttpRequestPtr &req,
     std::vector<News> news_list = mp.orderBy(News::Cols::_id).limit(15).offset(0).findAll();
     std::cout << news_list.size() << " rows 111111111111111111111111111!" << std::endl;
     //异步查询
-    auto f = clientPtr->execSqlAsyncFuture("select * from news order by create_time desc limit 15");
+    auto f = clientPtr->execSqlAsyncFuture("select id,title,create_time from news order by create_time desc limit 15");
     auto result = f.get();
 
     std::cout << typeid(result).name() << std::endl;
     std::cout << result.size() << " rows selected!" << std::endl;
 
+    Json::Value vl;
+    for (auto rr : news_list)
+    {
+        // std::cout << rr.toJson() << std::endl;
+        vl.append(rr.toJson());
+    }
+
+    Json::Value vt;
+    int i= 0;
+    for (auto rt : result)
+    {
+        vt[i]["id"] = rt["id"].as<std::string>();
+        vt[i]["title"] = rt["title"].as<std::string>();
+        vt[i]["create_time"] = rt["create_time"].as<std::string>();
+        i++;
+    }
+
+
     Json::Value ret;
     ret["result"] = "ok";
-    ret["news_list"] = news_list.data();
+    ret["news"] = vt;
+    // ret["news_list"] = vl;
+
     auto resp = HttpResponse::newHttpJsonResponse(ret);
     callback(resp);
 }
